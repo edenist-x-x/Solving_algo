1581ae981d68c13d64d63496468c42e2c11ca36d
edenist-x-x <yaron_e5@naver.com>
2021-03-23 23:01:04 +0900

    Add new Solved prob.

---
+++ src/BojAlgorithms/no11726.java
@@ -0,0 +1,24 @@
+package BojAlgorithms;
+
+import java.util.Scanner;
+
+public class no11726 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        if(n >= 2){
+            for(int i = 2; i <= n; i++){
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            }
+        }
+
+
+
+        System.out.println(dp[n]);
+    }
+}
